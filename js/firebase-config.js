4a0f8563d0f2b0af833170ced22ba45d06420a2f
Rituearth <sabafoundationforrarediseases@gmail.com>
2025-10-21 14:00:42 +0530

    media-upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDb5YyZOu4lWNGfJ5RK9SX_5bPq9nwfsnc",
   authDomain: "sfrd-organization.firebaseapp.com",
   projectId: "sfrd-organization",
-  storageBucket: "sfrd-organization.appspot.com",
+  storageBucket: "sfrd-organization.firebasestorage.app",
   messagingSenderId: "1022212374454",
   appId: "1:1022212374454:web:23b0d3f2c44db5b6b40b17",
   measurementId: "G-CP58WDH12M"
